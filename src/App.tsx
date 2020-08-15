dd68d638549d2c8dd304ef1876cc77ffe7b0db63
Felipe Leal <felipewanderson2020@gmail.com>
2020-08-15 02:11:01 -0300

    :lipstick: layout page SignIn
    create page signIn, your sctucture and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
+import SingnIn from './pages/SingnIn';
 
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <SingnIn />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
